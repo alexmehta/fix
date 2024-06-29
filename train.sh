939291a45614d9c8ffc332578653da532368c1fe
Alexander Mehta <alexandermehta@outlook.com>
2024-06-28 17:52:37 -0700

    add train.sh

---
+++ train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#$ -l h_rt=18:00:00
+
+#$ -m ea
+
+#$ -N DEQ-Prox-Denoising-Full
+# note do -l buyin
+#$ -j y
+#$ -pe omp 4
+#$ -l gpus=4
+#$ -l gpu_c=7.0
+module load python3/3.10.12
+module load torch/2.1
+# pip3 install -r requirements.txt --no-cache-dir
+python3 -m deep_equilibrium_inverse.scripts.fixedpoint.deblur_proxgrad_fixedeta_pre --batch_size 256
