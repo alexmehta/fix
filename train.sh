f53fe5a6969738fc8d97e795b85bb9214814f345
Alexander Mehta <alexandermehta@outlook.com>
2024-06-28 17:53:51 -0700

    fix dirs:

--- train.sh
+++ train.sh
@@ -13,4 +13,4 @@
 module load python3/3.10.12
 module load torch/2.1
 # pip3 install -r requirements.txt --no-cache-dir
-python3 -m deep_equilibrium_inverse.scripts.fixedpoint.deblur_proxgrad_fixedeta_pre --batch_size 256
+python3 -m deep_equilibrium_inverse.scripts.fixedpoint.deblur_proxgrad_fixedeta_pre --batch_size 256 --savepath models/blur.ckpt  --loadpath models/blur.ckpt --datapath ../img_align_celeba
